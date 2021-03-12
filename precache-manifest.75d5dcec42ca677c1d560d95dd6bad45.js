a0b0d27006afe7e1192c0d79a1f1adc1f249e1df
Virola <virola@yeah.net>
2021-03-12 10:09:36 +0000

    Automatically update from travis-ci

---
+++ precache-manifest.75d5dcec42ca677c1d560d95dd6bad45.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "643ad4dd1e1696a038c6",
+    "url": "css/app.c7a6dcee.css"
+  },
+  {
+    "revision": "acb4697c26c904601a4d",
+    "url": "css/chunk-vendors.cfc264db.css"
+  },
+  {
+    "revision": "3111014bc8ff571b4102039321eb62a2",
+    "url": "index.html"
+  },
+  {
+    "revision": "643ad4dd1e1696a038c6",
+    "url": "js/app.fbdac68d.js"
+  },
+  {
+    "revision": "acb4697c26c904601a4d",
+    "url": "js/chunk-vendors.a93a1c4b.js"
+  },
+  {
+    "revision": "e0e929a28f6348c4de52",
+    "url": "js/query.2123977d.js"
+  },
+  {
+    "revision": "79a4843b63382ee49c7a3a7e22317951",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
